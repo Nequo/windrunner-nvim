ecf2bcdb7d4920db1257d9c4db51e0387da162f0
Nadim Edde Gomez <nadim@eddegomez.org>
2024-10-06 19:28:20 +0100

    feat(all): init repo

---
+++ README.md
@@ -0,0 +1,11 @@
+# Windrunner
+
+Windrunner is a minimal colorscheme based on [Fogbell](https://github.com/jaredgorski/fogbell.vim)
+
+## Design
+
+The theme simply uses `vim.api.nvim_set_hl` to set highlight colors and does not aim to provide any customisability.
+Color is only used for non-syntax elements such as LSP Diagnostics, search and menus.
+
+## Screenshot
+
---
+++ lua/windrunner/colors.lua
@@ -0,0 +1,42 @@
+local colors = {}
+colors.dark = {
+    -- let s:voidBlack = {'gui': '#1C1C1C', 'cterm256': '234'}
+    -- let s:voidBlack2 = {'gui': '#262626', 'cterm256': '235'}
+    -- let s:voidGray1 = {'gui': '#3E3D32', 'cterm256': '237'}
+    -- let s:voidGray2 = {'gui': '#49483E', 'cterm256': '239'}
+    -- let s:voidGray3 = {'gui': '#6B6B6B', 'cterm256': '242'}
+    -- let s:voidGray4 = {'gui': '#B0B0B0', 'cterm256': '249'}
+    -- let s:voidSteel = {'gui': '#D0D0D0', 'cterm256': '252'}
+    -- let s:voidBlue = {'gui': '#699B9B', 'cterm256': '109'}
+    -- let s:voidGreen = {'gui': '#569F7A', 'cterm256': '72'}
+    -- let s:voidGold = {'gui': '#8E700B', 'cterm256': '136'}
+    -- let s:voidRed = {'gui': '#821A1A', 'cterm256': '1'}
+    -- let s:voidRed2 = {'gui': '#FF0000', 'cterm256': '196'}
+    -- let s:none = {'gui': 'NONE', 'cterm256': 'NONE'}
+    base00  = '#1C1C1C',
+    base01  = '#262626',
+    base02  = '#303030',
+    base03  = '#444444',
+    base04  = '#D0D0D0',
+    base05  = '#f2f4f8',
+    base06  = '#ffffff',
+    cyan    = '#699B9B',
+    blue    = '#91AADF',
+    purple  = '#CF73E6',
+    green   = '#57BA37',
+    yellow  = '#F0D50C',
+    orange  = '#F66100',
+    red     = '#821A1A',
+    grey    = '#6B6B6B',
+
+    none               = 'NONE',
+    Selection          = '#264F78',
+
+    DiffRedDark        = '#4B1818',
+    DiffRedLight       = '#6F1313',
+    DiffGreenDark      = '#373D29',
+    DiffGreenLight     = '#4B5632',
+
+}
+
+return colors
